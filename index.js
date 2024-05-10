e2376034485d340d3bdd0df036c5b0d3a9883dd0
asfasfasf121 <js1NiklasHL@hotmail.com>
2024-05-10 09:13:04 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
-  let hasCycle = false;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      hasCycle = true;
-      break;
-    }
-  }
-  if (!hasCycle) return null;
-  slow = head;
-  while (slow !== fast) {
-    slow = slow.next;
-    fast = fast.next;
-  }
-  return slow;
-}
+const deepFlatten = (arr) =>
+  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
