cbb372547d1c456b2f6e9fbb79ce8521da2954b0
asfasfasf121 <js1NiklasHL@hotmail.com>
2024-05-12 04:17:59 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const deepFlatten = (arr) =>
-  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
+function lengthOfLongestSubstring(s) {
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(left, map.get(s[right]) + 1);
+    }
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
+}
