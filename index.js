babd080f54824ebd13fc6222320ff7e818be9adc
asfasfasf121 <js1NiklasHL@hotmail.com>
2024-05-15 15:23:53 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 1; i <= n + 1; i++) {
-    first = first.next;
-  }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
-  }
-  second.next = second.next.next;
-  return dummy.next;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
